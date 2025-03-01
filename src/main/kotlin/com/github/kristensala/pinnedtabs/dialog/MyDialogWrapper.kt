cccddc53ca81be46df2d805bd27d46a221e14cae
Kristen Sala <salakristen1@gmail.com>
2025-03-01 22:15:49 +0200

    testing building a custom popup dialog

---
+++ src/main/kotlin/com/github/kristensala/pinnedtabs/dialog/MyDialogWrapper.kt
@@ -0,0 +1,105 @@
+package com.github.kristensala.pinnedtabs.dialog
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.openapi.ui.popup.JBPopup
+import com.intellij.openapi.ui.popup.PopupChooserBuilder
+import com.intellij.ui.components.JBList
+import java.awt.BorderLayout
+import java.awt.Dimension
+import java.awt.KeyEventDispatcher
+import java.awt.event.KeyEvent
+import javax.swing.JComponent
+import javax.swing.JLabel
+import javax.swing.JPanel
+
+class ShowDialogSampleAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        PinnedTabsPopupComponent().show();
+        /*if (PinnedTabs().isShowing) {
+            PinnedTabs().close(0)
+            return
+        }
+        PinnedTabs().show()*/
+    }
+}
+
+class PinFileAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        val currentProject = e.project;
+        if (currentProject != null) {
+            var path = currentProject.basePath;
+            val fem = FileEditorManager.getInstance(currentProject);
+            // we want to save current file into some sort of
+            // a local project file under .idea/ folder
+            val currentFile = fem.selectedFiles
+            if (currentFile.isNotEmpty()) {
+                //File(currentProject.projectFile).writeText(currentFile[0].path)
+            }
+        }
+
+    }
+
+}
+
+//https://plugins.jetbrains.com/docs/intellij/settings-tutorial.html#creating-the-appsettingscomponent-implementation
+//https://raw.githubusercontent.com/mustah/TabSwitch/refs/heads/master/src/org/intellij/ideaplugins/tabswitch/TabSwitchProjectComponent.java
+/*@State(
+    name = "org.intellij.sdk.settings.AppSettings",
+    storages = @Storage("SdkSettingsPlugin.xml")
+)*/
+
+class PinnedTabsPopupComponent() : KeyEventDispatcher {
+    private var popup: JBPopup? = null
+    private var list = JBList<String>()
+    private var builder: PopupChooserBuilder<String> = PopupChooserBuilder(this.list)
+
+    init {
+        this.builder
+            .setTitle("pinned tabs")
+            .setMovable(true)
+            .setResizable(true)
+    }
+
+    fun show() {
+        if (popup != null) {
+            return
+        }
+
+        popup = builder.createPopup()
+        popup?.showInFocusCenter()
+        //popup.showCenteredInCurrentWindow()
+    }
+
+    fun close() {
+        if (popup != null) {
+            popup?.dispose()
+            popup?.cancel()
+            popup = null
+        }
+    }
+
+    override fun dispatchKeyEvent(e: KeyEvent?): Boolean {
+        TODO("Not yet implemented")
+    }
+}
+
+class PinnedTabs : DialogWrapper(true) {
+    init {
+        init()
+        title = "Pinned Tabs"
+        isModal = false
+    }
+
+    override fun createCenterPanel(): JComponent {
+        val panel = JPanel(BorderLayout())
+        val label = JLabel("Pinned tabs")
+        label.preferredSize = Dimension(500, 200)
+        panel.add(label, BorderLayout.CENTER)
+
+        return panel
+    }
+
+}
